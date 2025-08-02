44d01f8bab4661c5d462dd75f7ca67fd513be944
Sudhanshu Mishra <sudhanshumishra@Sudhanshus-MacBook-Air.local>
2025-08-03 01:54:11 +0200

    Modified Index.js

--- Backend/index.js
+++ Backend/index.js
@@ -48,7 +48,7 @@ app.get('/all', async (req, res) => {
 
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
